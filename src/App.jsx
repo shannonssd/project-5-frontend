111a898ed64ef56d097e608648fdac9837832c75
shannonssd <shannonssd@gmail.com>
2022-02-15 15:23:49 +0800

    Added code for NavBar, App and store base structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import './App.css';
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";import { NeighbourhoodProvider } from './components/others/store';
+import './App.css';
+import NavBar from './components/others/NavBar';
+import Home from './components/pages/Home';
+
 
 export default function App() {
   return (
-    <div>
-    </div>
+    <NeighbourhoodProvider>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>  
+    </NeighbourhoodProvider>
   );
 }
