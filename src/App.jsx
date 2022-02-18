d716062ce0158ac567e2ae6a1e50dd6e1fca8ae7
Hsiu Ping Gay <hsiups@Hsius-MacBook-Pro.local>
2022-02-18 16:23:49 +0800

    added theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css';
 import React from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import mainTheme from './theme';
+import Item from './components/hand-me-down/ItemCard';
 
 export default function App() {
   return (
-    <div>
-    </div>
+    <ThemeProvider theme={mainTheme}>
+      <div>
+        <Item />
+      </div>
+    </ThemeProvider>
   );
 }
