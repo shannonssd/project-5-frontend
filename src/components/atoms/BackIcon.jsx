1ed00b384bae6b8859ffa48dd8c0dd6fb75ca6e0
Hsiu Ping Gay <hsiups@Hsius-MacBook-Pro.local>
2022-02-23 20:29:55 +0800

    back icon

---
+++ src/components/atoms/BackIcon.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  Button,
+} from '@mui/material';
+import { styled } from '@mui/material/styles';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+
+function BackIcon(props) {
+  return (
+    <div />
+  );
+}
+
+export default BackIcon;
