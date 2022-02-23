14d64ce86fd1119e6378cdb01efb2710aab8fd01
Hsiu Ping <85098526+hsiupinggay@users.noreply.github.com>
2022-02-23 20:11:39 +0800

    Revert "Feature/auth signup"

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,25 @@
-/* eslint-disable no-nested-ternary */
-/*
- * ========================================================
- *                       Imports
- * ========================================================
- */
-import "./App.css";
-import React, { useState } from "react";
+import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link,
-} from "react-router-dom";
-import { ThemeProvider } from "@mui/material/styles";
-import mainTheme from "./theme";
+} from "react-router-dom";import { NeighbourhoodProvider } from './components/others/store';
+import './App.css';
+import NavBar from './components/others/NavBar';
+import Home from './components/pages/Home';
 
-import { AuthProvider } from "./components/others/store";
-
-// import Pages components
-import AuthPage from "./components/pages/AuthPage";
-import ChatListPage from "./components/pages/ChatListPage";
-import ChatRoomPage from "./components/pages/ChatRoomPage";
-import HandDownAddPage from "./components/pages/HandDownAddPage";
-import HandDownListPage from "./components/pages/HandDownListPage";
-import HandDownDetailPage from "./components/pages/HandDownDetailPage";
-import HomePage from "./components/pages/HomePage";
-import InterestGroupDetailPage from "./components/pages/InterestGroupDetailPage";
-import InterestGroupListPage from "./components/pages/InterestGroupListPage";
-import NavBar from "./components/others/NavBar";
-
-/*
-* ========================================================
-*                       App
-* ========================================================
-*/
 
 export default function App() {
-  const [chosenItem, setChosenItem] = useState(null);
-
   return (
-    <Router>
-      <div>
-
-        <ThemeProvider theme={mainTheme}>
-          <AuthProvider>
-            <Switch>
-              <Route exact path="/">
-                <AuthPage />
-              </Route>
-              <Route exact path="/home">
-                <HomePage />
-              </Route>
-              <Route exact path="/hmd-list">
-                <HandDownListPage setChosenItem={setChosenItem} />
-              </Route>
-              <Route exact path="/hmd-detail">
-                <HandDownDetailPage chosenItem={chosenItem} />
-              </Route>
-              <Route exact path="/hmd-add">
-                <HandDownAddPage />
-              </Route>
-              <Route exact path="/chat-list">
-                <ChatListPage />
-              </Route>
-              <Route exact path="/chat-room">
-                <ChatRoomPage />
-              </Route>
-              <Route exact path="/interest-group-list">
-                <InterestGroupListPage />
-              </Route>
-              <Route exact path="/interest-group-detail">
-                <InterestGroupDetailPage />
-              </Route>
-            </Switch>
-          </AuthProvider>
-        </ThemeProvider>
-
-      </div>
-    </Router>
+    <NeighbourhoodProvider>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>  
+    </NeighbourhoodProvider>
   );
 }
